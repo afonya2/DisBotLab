6bfa3ced9bab843e09a4afbbe0e1e0c3db6d42b1
afonya <adamhir@freemail.hu>
2025-07-08 16:09:09 +0200

    Setup everything, begin testing

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-configű
+import { definePreset } from "@primeuix/themes";
+import Aura from "@primeuix/themes/aura";
+
+const customTheme = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: "#99b2e4",
+            100: "#84a2de",
+            200: "#7093d9",
+            300: "#5b83d3",
+            400: "#4774ce",
+            500: "#3264c8",
+            600: "#2850a0",
+            700: "#23468c",
+            800: "#193264",
+            900: "#0f1e3c",
+            950: "#0a1428",
+        },
+    },
+});
+
+export default defineNuxtConfig({
+    compatibilityDate: "2025-05-15",
+    devtools: { enabled: true },
+    modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
+    primevue: {
+        options: {
+            theme: {
+                preset: customTheme,
+            },
+        },
+        components: {
+            exclude: ["Form", "FormField"],
+        },
+    }
+});
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { VueFlow, type Node } from "@vue-flow/core";
+import "./index.css";
+
+const nodes = ref<Node[]>([
+    {
+        id: "1",
+        type: "input",
+        data: { label: "Node 1" },
+        position: { x: 250, y: 5 }
+    },
+    {
+        id: "2",
+        data: { label: "Node 2" },
+        position: { x: 100, y: 100 },
+    },
+    {
+        id: "3",
+        data: { label: "Node 3" },
+        position: { x: 400, y: 100 },
+    },
+    {
+        id: "4",
+        type: "output",
+        data: { label: "Node 4" },
+        position: { x: 400, y: 200 },
+    },
+]);
+
+const edges = ref([
+    { id: "e1-2", source: "1", target: "2" },
+    { id: "e1-3", source: "1", target: "3" },
+    { id: "e3-4", source: "3", target: "4" },
+]);
+</script>
+
+<template>
+    <VueFlow :nodes="nodes" :edges="edges" class="flow" fit-view-on-init>
+    </VueFlow>
+</template>
