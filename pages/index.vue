f80b036ff469a3c6c7620dbb66be5580fb6b379a
afonya <adamhir@freemail.hu>
2025-07-08 21:55:56 +0200

    designed the main menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,57 @@
 <script setup lang="ts">
+    import { Menu, Divider } from 'primevue';
+    import type { MenuItem } from 'primevue/menuitem';
+    import { ref } from 'vue';
+    import 'primeicons/primeicons.css'
 
+    useHead({
+        title: 'DisBotLab',
+    })
+    const menuItems = ref<MenuItem[]>([
+        {
+            label: 'Dashboard',
+            items: [
+                { label: 'Home', icon: 'pi pi-home' },
+                { label: 'Modules', icon: 'pi pi-file' }
+            ]
+        },
+        {
+            label: 'Administration',
+            items: [
+                { label: 'Users', icon: 'pi pi-users' },
+                { label: 'Updates', icon: 'pi pi-refresh' },
+                { label: 'Logs', icon: 'pi pi-list' },
+                { label: 'Settings', icon: 'pi pi-cog' },
+            ]
+        },
+        {
+            label: 'DBL',
+            items: [
+                { label: 'Github', icon: 'pi pi-github', url: 'https://github.com/afonya2/DisBotLab', target: '_blank' },
+                { label: 'Issues', icon: 'pi pi-exclamation-triangle', url: 'https://github.com/afonya2/DisBotLab/Issues', target: '_blank' }
+            ]
+        },
+        {
+            label: 'User',
+            items: [
+                { label: 'Example User', icon: 'pi pi-user' },
+                { label: 'Sign out', icon: 'pi pi-sign-out' }
+            ]
+        }
+    ])
 </script>
 
 <template>
-
+    <Menu :model="menuItems" class="w-fit h-screen">
+        <template #start>
+            <img src="/logo.svg" alt="logo">
+            <Divider />
+        </template>
+    </Menu>
 </template>
 
 <style scoped>
-
+    img {
+        width: 200px;
+    }
 </style>
